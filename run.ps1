92f28e12f7d9dbf2504a907be537e0779f405e9c
Dave Risney <dave@deletethis.net>
2016-06-03 22:29:22 -0700

    Add IE and IE support

--- run.ps1
+++ run.ps1
@@ -1,18 +1,14 @@
 param(
-    [ValidateSet("edge", "chrome", "firefox", "opera")] [string] $White = "edge",
-    [ValidateSet("edge", "chrome", "firefox", "opera")] [string] $Black = "chrome");
+    [ValidateSet("edge", "chrome", "firefox", "opera", "ie")] [string] $White = "edge",
+    [ValidateSet("edge", "chrome", "firefox", "opera", "ie")] [string] $Black = "chrome");
 
 $configs = @{
-    edge =      @(@("MicrosoftEdge*", "MicrosoftWebDriver"),    "Start-Process -FilePath `"C:\program files (x86)\Microsoft Web Driver\MicrosoftWebDriver.exe`" -ArgumentList `"--verbose --port={0}`";");
-    chrome =    @(@("chrome", "chromedriver"),                  "Start-Process -FilePath `"C:\program files (x86)\Microsoft Web Driver\chromedriver.exe`" -ArgumentList `"--verbose --port={0}`";");
-    opera =     @(@("opera", "operadriver"),                    "Start-Process -FilePath `"C:\program files (x86)\Microsoft Web Driver\operadriver.exe`" -ArgumentList `"--verbose --port={0}`";");
-    firefox =   @(@("firefox", "wires"),                        "Start-Process -FilePath `"C:\program files (x86)\Microsoft Web Driver\wires.exe`" -ArgumentList '-b `"C:\Program Files\nightly\firefox.exe`" --webdriver-port {0}' ");
+    ie =        "Start-Process -FilePath `"C:\Program Files (x86)\Microsoft Web Driver\IEDriverServer.exe`" -ArgumentList `"/port={0}`";";
+    edge =      "Start-Process -FilePath `"C:\program files (x86)\Microsoft Web Driver\MicrosoftWebDriver.exe`" -ArgumentList `"--verbose --port={0}`";";
+    chrome =    "Start-Process -FilePath `"C:\program files (x86)\Microsoft Web Driver\chromedriver.exe`" -ArgumentList `"--verbose --port={0}`";";
+    opera =     "Start-Process -FilePath `"C:\program files (x86)\Microsoft Web Driver\operadriver.exe`" -ArgumentList `"--verbose --port={0}`";";
+    firefox =   "Start-Process -FilePath `"C:\program files (x86)\Microsoft Web Driver\wires.exe`" -ArgumentList '-b `"C:\Program Files\nightly\firefox.exe`" --webdriver-port {0}';";
 };
-
-$White
-$configs[$White];
-$Black
-$configs[$Black];
 
 ps | ?{ 
     $psName = $_.name;
@@ -21,7 +17,8 @@ ps | ?{
         "MicrosoftEdge*", "MicrosoftWebDriver",
         "chrome", "chromedriver",
         "opera", "operadriver",
-        "firefox", "wires"
+        "firefox", "wires",
+        "iexplore", "IEDriverServer"
     ) | ?{ $psName -like $_; }
 } | kill;
 
@@ -30,6 +27,6 @@ $blackPort = 17558;
 $chessPort = 17559;
 
 Start-Process -FilePath $env:UserProfile\appdata\roaming\npm\http-server.cmd -WorkingDirectory $env:UserProfile\development\webdriverchess\simplechess -ArgumentList "-p $chessPort"
-iex ($configs[$White][1] -f $whitePort);
-iex ($configs[$Black][1] -f $blackPort);
+iex ($configs[$White] -f $whitePort);
+iex ($configs[$Black] -f $blackPort);
 node driver.js http://localhost:$whitePort http://localhost:$blackPort http://localhost:$chessPort
